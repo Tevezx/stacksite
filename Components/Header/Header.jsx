288d30a9cdcf326fa0d43e579d72349f2d99ff81
Tevezx <teveztwitch@gmail.com>
2024-10-14 13:44:30 -0300

    Adicionando elementos na Header do site

--- Components/Header/Header.jsx
+++ Components/Header/Header.jsx
@@ -1,5 +1,35 @@
+import { useState } from 'react';
 import '../../Style/Header/Header.css';
+import logotipo from '../../src/assets/logotipo.png';
+import menuIcon from '../../src/assets/menu.png';
 
-export default function Header(){
-    
+export default function Header() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <header>
+            <nav className='navbar'>
+                <div className="logo-container">
+                    <img className='logo' src={logotipo} alt='Logo' />
+                </div>
+                <div className="menu-toggle-container">
+                    <button className='menu-toggle' onClick={toggleMenu}>
+                        <img src={menuIcon} alt='Menu Icon'/>
+                    </button>
+                </div>
+                {isOpen && (
+                    <div className='menu'>
+                        <a href="#" onClick={toggleMenu}>Home</a>
+                        <a href="#" onClick={toggleMenu}>Sobre Nossa Empresa</a>
+                        <a href="#" onClick={toggleMenu}>Nossa Equipe</a>
+                        <a href="#" onClick={toggleMenu}>Soluções</a>
+                    </div>
+                )}
+            </nav>
+        </header>
+    );
 }
