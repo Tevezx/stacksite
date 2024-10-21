4be45180031406c5cac969bcd5422be228de971e
Tevezx <teveztwitch@gmail.com>
2024-10-21 20:20:57 -0300

    Uptade Home

--- Components/Home/Home.jsx
+++ Components/Home/Home.jsx
@@ -2,7 +2,7 @@ import '../../Style/Home/Home.css';
 import estrela from '../../src/assets/estrela.png';
 import fundo from '../../src/assets/fundo.png';
 import logo_home from '../../src/assets/logo_home.png';
-import degrade from '../../src/assets/degrade.png';
+
 import estrela_pequena from '../../src/assets/estrela_pequena.png'
 
 export default function Home() {
@@ -15,8 +15,7 @@ export default function Home() {
                 <img src={logo_home} alt='home'/>
                 <p className='descricao_logo'>Lorem Ipsumis simply dummy text of the printing and typesetting industry.</p>
                 <a href='#'>See more Informations ➜</a>
-            </div>  
->>>>>>> 50396a62466897653b5f5dcdfd8a313d5472230b
+            </div> 
             <div className='estrela2'>
                 <img src={estrela_pequena} alt='estrela2'/>
             </div>
