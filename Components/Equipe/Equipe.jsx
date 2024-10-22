846cee29637ad4670883507677942e0609c7e296
Tevezx <teveztwitch@gmail.com>
2024-10-22 19:42:02 -0300

    Alterando o estilo da página Equipe

--- Components/Equipe/Equipe.jsx
+++ Components/Equipe/Equipe.jsx
@@ -1,7 +1,27 @@
-export default function Equipe(){
-    return(
-        <div>
-            
-        </div>
-    )
+import '../../Style/Equipe/Equipe.css';
+import linha from '../../src/assets/linha.png';
+import javascript from '../../src/assets/javas.png';
+import node from '../../src/assets/node.png';
+import css from '../../src/assets/css.png';
+import sql from '../../src/assets/sql.png';
+import react from '../../src/assets/react.svg';
+
+export default function Equipe() {
+    return (
+        <section className="equipe-container">
+            <div className="section-equipe">
+                <div className="linha_equipe">
+                    <img src={linha} alt="Linha decorativa no topo" />
+                </div>
+                <div className="text-equipe">
+                    <h1>
+                        <span className="text-conheca">Conheça as</span>
+                        <span className="text-tecnologia"> Tecnologias</span>
+                        <span className="text-usadas"> usadas</span>
+                        <span className="text-nossa"> pela nossa equipe</span>
+                    </h1>
+                </div>
+            </div>
+        </section>
+    );
 }
