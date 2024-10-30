d7fe4515a674ae575d7585fc8580b13556c672e6
Tevezx <teveztwitch@gmail.com>
2024-10-30 20:50:27 -0300

    Atualizando Rotas

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,18 @@ import Empresa from '../Components/Empresa/Empresa';
 import Contato from '../Components/Contato/Contato';
 import Equipe from '../Components/Equipe/Equipe';
 import Solucoes from '../Components/Solucoes/Solucoes';
+import ScrollToTop from '../Components/ScrollToTop';
 
 function App() {
   return (
     <Router basename="/stacksite">
+      <ScrollToTop />
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/empresa" element={<Empresa />} />
         <Route path="/equipe" element={<Equipe />} />
-        <Route path="/solucoes" element={<Solucoes/>} />
+        <Route path="/solucoes" element={<Solucoes />} />
         <Route path="/contato" element={<Contato />} />
       </Routes>
       <Footer />
