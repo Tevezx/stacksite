12aa7d81fb7048858050d885e989f894efd0875c
Tevezx <teveztwitch@gmail.com>
2024-11-01 20:37:55 -0300

    Atualização do footer responsivo

--- Components/Footer/Footer.jsx
+++ Components/Footer/Footer.jsx
@@ -6,24 +6,22 @@ import facebook from '../../src/assets/facebook.png';
 import github from '../../src/assets/github.png';
 import { Link } from 'react-router-dom';
 
-export default function Footer(){
-    return(
+export default function Footer() {
+    return (
         <footer>
-            <div className="frame1">
-                <img className="logo_footer" src={logo_footer} alt="logo footer" />
-                <span className="titulo_img">STACK.</span>
-                <span className="subtitulo_img">Empilhando Inovação</span>
-            </div>
             <div className="footer-container">
+                <div className="frame1">
+                    <img className="logo_footer" src={logo_footer} alt="logo footer" />
+                    <span className="titulo_img">STACK.</span>
+                    <span className="subtitulo_img">Empilhando Inovação</span>
+                </div>
                 <div className="nav-bar">
                     <span className="titulo_link">Links</span>
-                    <ul className="lista">
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/empresa">Sobre nossa empresa</Link></li>
-                        <li><Link to="/equipe">Nossa equipe</Link></li>
-                        <li><Link to="/solucoes">Soluções</Link></li>
-                        <li><Link to="/contato">Contato</Link></li>
-                    </ul>
+                        <li className='lista'><Link to="/">Home</Link></li>
+                        <li className='lista'><Link to="/empresa">Sobre nossa empresa</Link></li>
+                        <li className='lista'><Link to="/equipe">Nossa equipe</Link></li>
+                        <li className='lista'><Link to="/solucoes">Soluções</Link></li>
+                        <li className='lista'><Link to="/contato">Contato</Link></li>
                 </div>
                 <div className="footer-contact">
                     <span className="contact">Fale Conosco</span>
@@ -32,15 +30,23 @@ export default function Footer(){
                     </p>
                 </div>
                 <div className="footer-social">
-                    <a href="https://facebook.com"><img src={facebook} alt="Facebook"/></a>
-                    <a href="https://instagram.com"><img src={instagram} alt="Instagram"/></a>
-                    <a href="https://github.com"><img src={github} alt="GitHub"/></a>
-                    <a href="https://linkedin.com"><img src={linkedln} alt="LinkedIn"/></a>
-                </div>
-                <div className="footer-copyright">
-                    <p className='copyryght'>© 2024 Copyright by Stack Corp®. All rights reserved.</p>
+                    <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+                        <img src={facebook} alt="Facebook" />
+                    </a>
+                    <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+                        <img src={instagram} alt="Instagram" />
+                    </a>
+                    <a href="https://github.com" target="_blank" rel="noopener noreferrer">
+                        <img src={github} alt="GitHub" />
+                    </a>
+                    <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+                        <img src={linkedln} alt="LinkedIn" />
+                    </a>
                 </div>
             </div>
+            <div className="footer-copyright">
+                <p className='copyryght'>© 2024 Copyright by Stack Corp®. All rights reserved.</p>
+            </div>
         </footer>
-    )
+    );
 }
