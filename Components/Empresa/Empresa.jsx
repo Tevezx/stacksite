45d4826819f268fc0ce8718623dafd419a6bdd8c
Tevezx <teveztwitch@gmail.com>
2024-11-02 13:27:39 -0300

    Inserindo a responsividade da tela empresa

--- Components/Empresa/Empresa.jsx
+++ Components/Empresa/Empresa.jsx
@@ -7,10 +7,10 @@ export default function Empresa() {
     return (
         <section className="empresa-container">
             <div className="section-content">
-                <div className="linha">
-                    <img src={linha} alt="Linha decorativa no topo" />
-                </div>
                 <div className="text-content">
+                    <div className="linha">
+                        <img src={linha} alt="Linha decorativa no topo" />
+                    </div>
                     <h2>Conheça um pouco mais</h2>
                     <h1>
                         <span className="sobre-nossa">Sobre Nossa</span>
@@ -21,29 +21,31 @@ export default function Empresa() {
                         simbolizando a acumulação de soluções e conhecimentos. A nossa missão é 
                         proporcionar os mais diversos softwares que atendam e satisfaçam as necessidades 
                         do usuário, tornando algumas tarefas do cotidiano mais simples!
-                    </p>
+                    </p>       
                 </div>
                 <div className="image-content">
                     <img src={image2} alt="Trabalhando em equipe" />
                 </div>
             </div>
-            <div className="linha2">
-                <img src={linha} alt="Linha decorativa no topo" />
-            </div>
-            <div className="text-content direita">
-                <h2>Conheça um pouco mais</h2>
-                <h1>
-                    <span className="sobre-nossa">Sobre Nossa</span>
-                    <span className="empresa-gradient"> Empresa</span>
-                </h1>
-                <p>
-                    A visão da empresa é ser reconhecida por sua excelência em acumular soluções, 
-                    conhecimentos e tecnologias. Nossos principais valores são a Honestidade, Qualidade, 
-                    Liderança, Paixão, Impacto, Comunicação e Responsabilidade.
-                </p>
-            </div>
-            <div className="image-content direita">
-                <img src={image1} alt="Discussão de ideias" />
+            <div className="section-content">    
+                <div className="text-content direita">
+                    <div className="linha2">
+                        <img src={linha} alt="Linha decorativa no topo" />
+                    </div>
+                    <h2>Conheça um pouco mais</h2>
+                    <h1>
+                        <span className="sobre-nossa">Sobre Nossa</span>
+                        <span className="empresa-gradient"> Empresa</span>
+                    </h1>
+                    <p>
+                        A visão da empresa é ser reconhecida por sua excelência em acumular soluções, 
+                        conhecimentos e tecnologias. Nossos principais valores são a Honestidade, Qualidade, 
+                        Liderança, Paixão, Impacto, Comunicação e Responsabilidade.
+                    </p>
+                </div>                    
+                <div className="image-content esquerda">
+                    <img src={image1} alt="Discussão de ideias" />
+                </div>   
             </div>
         </section>
     );
