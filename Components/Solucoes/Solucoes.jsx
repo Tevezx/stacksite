a95972ed51bfa66d475f62efe70b060c34bab517
Tevezx <teveztwitch@gmail.com>
2024-11-06 00:39:43 -0300

    Atualizando responsividade da tela Contato e Home

--- Components/Solucoes/Solucoes.jsx
+++ Components/Solucoes/Solucoes.jsx
@@ -14,7 +14,7 @@ export default function Solucoes() {
                 O que são <span className='txtSolucao'>Soluções</span>?
             </span>
             <p className='textInicial'>
-                Nossas soluções são aplicativos desenvolvidos para facilitar o dia a dia e resolver problemas comuns de forma prática e eficiente...
+            Nossas soluções são aplicativos desenvolvidos para facilitar o dia a dia e resolver problemas comuns de forma prática e eficiente. Eles foram projetados para atender às necessidades dos usuários, oferecendo funcionalidades que simplificam tarefas cotidianas, Com uma interface intuitiva e recursos personalizados, nossos aplicativos visam não apenas economizar tempo, mas também proporcionar mais qualidade de vida, permitindo que você se concentre no que realmente importa. A tecnologia a serviço da sua rotina!
             </p>
             <img src={linha_solucoes} className='linha_solucoes' alt="Linha Soluções" />
             <span className='titulo2_solucoes'>
@@ -22,7 +22,7 @@ export default function Solucoes() {
             </span>
             <img className='img_telas_app' src={img_telas_app} alt="Tela do Aplicativo" />
             <span className='titulo_mgp'>MGP - Meu Guia Pessoal</span>
-            <p className='descricao_mgp'>Born out of a vision, a single-minded objective that puts service before anything else, Swift Clearance and Forwarding Corp. surging forth to deliver the best services in the shipping and logistics scenario. Its meteoric rise stems out of a solid foundation. The management boasts of over 20 years of rich and varied experience in the shipping and freight forwarding industry.</p>
+            <p className='descricao_mgp'>Inspirado por uma visão focada em oferecer o melhor serviço possível, o MGP - Meu Guia Pessoal oferece as melhores soluções, nosso aplicativo se destaca pela sua interface intuitiva e recursos personalizados que tornam a navegação rápida e eficiente.</p>
             <a className='btn_link' style={{ fontSize: 14 }} href="https://github.com" target="_blank" rel="noopener noreferrer">
                 Visualizar mais no Git-Hub <img className='img_vector' src={vector} alt="Vector" />
             </a>
